6005c30ea179e387432275ae52cd379e67225a3d
Mike Zrimsek <mikezrimsek@gmail.com>
2017-03-27 03:56:01 -0400

    add interface doc comments

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -1,15 +1,18 @@
 package ast
 
+// Node : Generic AST node
 type Node interface {
 	TokenLiteral() string
 	String() string
 }
 
+// Statement : Generic AST statement node
 type Statement interface {
 	Node
 	statementNode()
 }
 
+// Expression : Generic AST expression node
 type Expression interface {
 	Node
 	expressionNode()
