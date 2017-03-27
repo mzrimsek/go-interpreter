c990b4602a7732ddf5d6abc41f002fd838a907a1
Mike Zrimsek <mikezrimsek@gmail.com>
2017-03-27 04:14:04 -0400

    added program doc comments

--- src/monkey/ast/program.go
+++ src/monkey/ast/program.go
@@ -2,16 +2,17 @@ package ast
 
 import "bytes"
 
+// Program : Top level construct containing all parsed AST nodes
 type Program struct {
 	Statements []Statement
 }
 
+// TokenLiteral : Returns the literal representations of the tokens for each of the program's statements
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
-	} else {
-		return ""
 	}
+	return ""
 }
 
 func (p *Program) String() string {
