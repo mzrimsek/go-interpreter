578c53f3f0199eb794f60e7c66b2b79a03467d02
Mike Zrimsek <mikezrimsek@gmail.com>
2017-03-27 02:44:56 -0400

    add some fun error stuff
    parser finished for now - on to evaluation

--- src/monkey/repl/repl.go
+++ src/monkey/repl/repl.go
@@ -9,6 +9,18 @@ import (
 )
 
 const PROMPT = ">> "
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -36,7 +48,10 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
-		io.WriteString(out, "\t"+msg+"\t")
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
