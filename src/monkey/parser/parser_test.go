41ea167b8ba1e2e0d58a1599b616550b46257df0
Mike Zrimsek <mikezrimsek@gmail.com>
2017-03-31 21:19:07 -0400

    add len built in function

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -657,7 +657,7 @@ func testLiteralExpression(t *testing.T, exp ast.Expression, expected interface{
 func testInfixExpression(t *testing.T, exp ast.Expression, left interface{}, operator string, right interface{}) bool {
 	opExp, ok := exp.(*ast.InfixExpression)
 	if !ok {
-		t.Errorf("exp is not ast.OperatorExpression. got=%T(%s)", exp, exp)
+		t.Errorf("exp is not ast.OperatorExpression. got=%T (%s)", exp, exp)
 		return false
 	}
 
