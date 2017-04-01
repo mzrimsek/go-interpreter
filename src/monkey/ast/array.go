91a44f4aa5b3bb5721a7a1461285e2fda2ba294b
Mike Zrimsek <mikezrimsek@gmail.com>
2017-03-31 22:29:45 -0400

    added parsing array literals

---
+++ src/monkey/ast/array.go
@@ -0,0 +1,33 @@
+package ast
+
+import "monkey/token"
+import "bytes"
+import "strings"
+
+// ArrayLiteral : Expression node representing an array of elements
+type ArrayLiteral struct {
+	Token    token.Token
+	Elements []Expression
+}
+
+func (al *ArrayLiteral) expressionNode() {}
+
+// TokenLiteral : Returns the literal representation of the token
+func (al *ArrayLiteral) TokenLiteral() string {
+	return al.Token.Literal
+}
+
+func (al *ArrayLiteral) String() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, el := range al.Elements {
+		elements = append(elements, el.String())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}
