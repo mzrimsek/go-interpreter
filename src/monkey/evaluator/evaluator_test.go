f2325ecba4d23d0a849c5435fab21406c38563c1
Mike Zrimsek <mikezrimsek@gmail.com>
2017-04-24 20:35:38 -0400

    split out expression evaluating and tests into separate files

--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -154,50 +154,6 @@ func TestEvalBooleanExpressions(t *testing.T) {
 	}
 }
 
-func TestBangOperator(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected bool
-	}{
-		{"!true", false},
-		{"!false", true},
-		{"!5", false},
-		{"!!true", true},
-		{"!!false", false},
-		{"!!5", true},
-	}
-
-	for _, tt := range tests {
-		evaluated := testEval(tt.input)
-		testBooleanObject(t, evaluated, tt.expected)
-	}
-}
-
-func TestIfElseExpressions(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected interface{}
-	}{
-		{"if (true) { 10 }", 10},
-		{"if (false) { 10 }", nil},
-		{"if (1) { 10 }", 10},
-		{"if (1 < 2) { 10 }", 10},
-		{"if (1 > 2) { 10 }", nil},
-		{"if (1 > 2) { 10 } else { 20 }", 20},
-		{"if (1 < 2) { 10 } else { 20 }", 10},
-	}
-
-	for _, tt := range tests {
-		evaluated := testEval(tt.input)
-		integer, ok := tt.expected.(int)
-		if ok {
-			testIntegerObject(t, evaluated, int64(integer))
-		} else {
-			testNullObject(t, evaluated)
-		}
-	}
-}
-
 func TestReturnStatements(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -474,34 +430,6 @@ func TestArrayLiterals(t *testing.T) {
 	testStringObject(t, result.Elements[3], "hello")
 }
 
-func TestArrayIndexExpressions(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected interface{}
-	}{
-		{"[1, 2, 3][0]", 1},
-		{"[1, 2, 3][1]", 2},
-		{"[1, 2, 3][2]", 3},
-		{"let i = 0; [1][i];", 1},
-		{"[1, 2, 3][1 + 1];", 3},
-		{"let myArray = [1, 2, 3]; myArray[2];", 3},
-		{"let myArray = [1, 2, 3]; myArray[0] + myArray[1] + myArray[2];", 6},
-		{"let myArray = [1, 2, 3]; let i = myArray[0]; myArray[i]", 2},
-		{"[1, 2, 3][3]", nil},
-		{"[1, 2, 3][-1]", nil},
-	}
-
-	for _, tt := range tests {
-		evaluated := testEval(tt.input)
-		integer, ok := tt.expected.(int)
-		if ok {
-			testIntegerObject(t, evaluated, int64(integer))
-		} else {
-			testNullObject(t, evaluated)
-		}
-	}
-}
-
 func TestHashLiterals(t *testing.T) {
 	input := `let two = "two";
 			  {
@@ -539,74 +467,6 @@ func TestHashLiterals(t *testing.T) {
 		}
 
 		testIntegerObject(t, pair.Value, expectedValue)
-	}
-}
-
-func TestHashIndexExpressions(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected interface{}
-	}{
-		{`{"foo": 5}["foo"]`, 5},
-		{`{"foo": 5}["bar"]`, nil},
-		{`let key = "foo"; {"foo": 5}[key]`, 5},
-		{`{}["foo"]`, nil},
-		{`{5: 5}[5]`, 5},
-		{`{true: 5}[true]`, 5},
-		{`{false: 5}[false]`, 5},
-	}
-
-	for _, tt := range tests {
-		evaluated := testEval(tt.input)
-		integer, ok := tt.expected.(int)
-		if ok {
-			testIntegerObject(t, evaluated, int64(integer))
-		} else {
-			testNullObject(t, evaluated)
-		}
-	}
-}
-
-func TestWhileExpressions(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected interface{}
-	}{
-		{`let x = 0;
-		  while(x < 2){
-			++x;
-		  }
-		  x;`, 2},
-	}
-
-	for _, tt := range tests {
-		evaluated := testEval(tt.input)
-		integer, ok := tt.expected.(int)
-		if ok {
-			testIntegerObject(t, evaluated, int64(integer))
-		} else {
-			testNullObject(t, evaluated)
-		}
-	}
-}
-
-func TestPostIncrementDecrementAndPreIncrementDecrement(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected interface{}
-	}{
-		{"let i = 10; let j = ++i; let k = i++; i == 12 && j == 11 && k == 11", true},
-		{"let i = 10; let j = --i; let k = i--; i == 8 && j == 9 && k == 9", true},
-	}
-
-	for _, tt := range tests {
-		evaluated := testEval(tt.input)
-		bool, ok := tt.expected.(bool)
-		if ok {
-			testBooleanObject(t, evaluated, bool)
-		} else {
-			testNullObject(t, evaluated)
-		}
 	}
 }
 
